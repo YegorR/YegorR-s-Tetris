7b5884b4b16dfdd91241d232892e45bf2bfe1d5d
YegorR <yegorryazancev1908@mail.ru>
2018-08-01 19:58:03 +0700

    Possibility to begin a new game is added.

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ class Game:
     def __init__(self):
         self._display_surf = None
         self._running = True
+        self._except_new_game = False
         self._background = None
         self._logic = None
         self._font = None
+        self._end_font = None
 
     def init(self):
         pygame.init()
         pygame.display.set_caption(constant.WINDOW_NAME)
         self._display_surf = pygame.display.set_mode((constant.WINDOW_WIDTH, constant.WINDOW_HEIGHT))
         self._running = True
+        self._except_new_game = False
         self._background = pygame.image.load(constant.FILE_BACKGROUND).convert()
         self._logic = logic.Logic()
         self._logic.start()
 
         self._font = pygame.font.SysFont("Arial", 20)
+        self._end_font = pygame.font.SysFont("Arial", 30)
 
     def event(self):
         for event in pygame.event.get():
@@ -28,6 +32,9 @@ class Game:
                 self._running = False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self._running = False
+
+            elif self._except_new_game and event.type == pygame.KEYDOWN:
+                self.new_game()
 
             elif event.type == constant.GAME_PERIOD_EVENT:
                 self._logic.period()
@@ -52,6 +59,10 @@ class Game:
         self._display_surf.blit(self._background, (0, 0))
         self._logic.render(self._display_surf)
         self._display_surf.blit(self.render_score(), (220, 130))
+        if self._except_new_game:
+            label = self.render_end_labels()
+            self._display_surf.blit(label[0], (50, 10))
+            self._display_surf.blit(label[1], (20, 350))
         pygame.display.update()
 
     def destroy(self):
@@ -66,11 +77,19 @@ class Game:
         self.destroy()
 
     def game_over(self):
-        self._running = False
-        print("GAME OVER")
+        self._except_new_game = True
 
     def render_score(self):
         return self._font.render(str(self._logic.get_score()), 1, (200, 10, 10))
+
+    def render_end_labels(self):
+        return (self._end_font.render("GAME OVER", 1, (200, 10, 10)),
+                self._end_font.render("Press any button!", 1, (200, 10, 10)))
+
+    def new_game(self):
+        self._logic = logic.Logic()
+        self._logic.start()
+        self._except_new_game = False
 
 
 if __name__ == "__main__":
